28cbc159d336e196bb32abea5995e5551a7f7f3e
paulpelykh <pelykhpaul@gmail.com>
2024-08-02 12:41:50 +0300

    feat: created Storage Factory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -2,31 +2,28 @@
 
 pragma solidity ^0.8.19;
 
-// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
-        // SimpleStorage simpleStorage = new SimpleStorage();
-        listOfSimpleStorageContracts.push(simpleStorageContractVariable);
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
     }
 
-    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)
-        public
-    {
-        // Address
+    function sfStorage(
+        uint256 _simpleStorageIndex,
+        uint256 _newSimpleStorageNumber
+    ) public {
+        // Adress
         // ABI
-        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
         listOfSimpleStorageContracts[_simpleStorageIndex].store(
-            _simpleStorageNumber
+            _newSimpleStorageNumber
         );
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
-        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
         return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
